baa37ff45cc73d4a121632f0c17649ef4621d388
Ashley Donald <ashd32@gmail.com>
2019-11-26 11:56:18 -0500

    refactors app for deployment

---
+++ build/precache-manifest.c63a3fdb1a9a3b2751e75f8c1beffbfb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2582a725270a2912937639f16ae242b3",
+    "url": "/Role-Based-Autorization/index.html"
+  },
+  {
+    "revision": "1826a024a0c70878af9d",
+    "url": "/Role-Based-Autorization/static/js/2.abd6c08e.chunk.js"
+  },
+  {
+    "revision": "6bb1f880c51ee4fb1567",
+    "url": "/Role-Based-Autorization/static/js/main.eec32d68.chunk.js"
+  },
+  {
+    "revision": "5a0c72e0e31478338fa8",
+    "url": "/Role-Based-Autorization/static/js/runtime-main.dcfdc552.js"
+  }
+]);
